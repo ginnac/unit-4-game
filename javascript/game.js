9ca77661ec21a7a9a3b0a041a4e5e747fd95b804
ginnac <ginnacampbell1990@gmail.com>
2019-06-01 23:27:49 -0600

    Brainstorming -  comments

--- javascript/game.js
+++ javascript/game.js
@@ -1,4 +1,62 @@
+// script game.js will take into effect when DOM has loaded...
 $(document).ready(function(){
+
+    //....................lets generate the randomBaseNumber between 19-120 and write it in id="number-to-score".......................
+
+        //create a loop between number 19-120 and create and array with those values (probably will have to push the values to an empty array)......
+            
+            //maybe inside (or outside) the loop lets choose a random one and then write it in the DOM this has to have a variable becuase we will use it 
+            //later for conditionals to see if user won or lost.
+
+    
+    //There is 4 diamonts (4 elements); here we will assign values to each diamond and values change after winning or losing
+
+                    //maybe function????
+                        //maybe conditional???
+                            //lets create a loop for first diamond between number 1 and 12 
+                            //and an array with those values (probably will have to push the values to an empty array)...
+
+                                //maybe inside (or outside) the loop lets choose a random one and then write it in the DOM this 
+                                //has to have a variable becuase we will use it  later for conditionals to see if user won or lost.
+
+
+                                        //probably we will have to remove last value from array so second diamond can be different than 1st value
+
+                                            //theh have 2nd diamond choose a random one from updated array
+
+
+                                                //then remove this value from array
+
+
+                                        //probably we will have to remove last value from array so 3rd diamond can be different than 2nd and 1st value
+
+                                            //theh have 3rd diamond choose a random one from updated array
+
+
+                                                //then remove this value from array
+
+
+                                        
+                                        
+                                        
+                                        //probably we will have to remove last value from array so 4th diamond can be different than 3rd, 2nd and 1st value
+
+                                            //theh have 4th diamond choose a random one from updated array
+
+
+                                                //then remove this value from the array
+
+
+                          
+                            //now create conditionals to accumulate the points each diamond gives us when user .on("click")......
+
+                                            //....... (need to brainstrorm here)
+                                    
+            
+
+
+
+
 
 
 });
